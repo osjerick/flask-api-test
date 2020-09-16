6df81b6b8b399b4c63403f8d05f82e8d3da06d22
Jerick Órdenes <os.jerick@gmail.com>
2020-09-15 20:37:13 -0400

    Add env vars for the API

--- build_local.sh
+++ build_local.sh
@@ -3,4 +3,10 @@
 export API_PORT=5000
 docker container rm --force api
 docker image build -t osjerick/api-test:latest .
-docker container run --detach --name api -p ${API_PORT}:${API_PORT} -e PORT=${API_PORT} osjerick/api-test:latest
+docker container run --detach --name api -p ${API_PORT}:${API_PORT} \
+-e PORT=${API_PORT} \
+-e AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+-e AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+-e SQS_REGION_NAME="${SQS_REGION_NAME}" \
+-e SQS_QUEUE_URL="${SQS_QUEUE_URL}" \
+osjerick/api-test:latest
